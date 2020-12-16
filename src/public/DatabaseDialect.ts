d012ac3081430b691da558e7abbbdb074f3f8836
Leonard Parisi <email@leonardparisi.com>
2020-12-16 12:24:30 -0500

    Added comments and updated readme

--- src/public/DatabaseDialect.ts
+++ src/public/DatabaseDialect.ts
@@ -1,1 +1,2 @@
+// The possible database dialects supported
 export type DatabaseDialect = 'mysql' | 'mssql' | 'sqlite' | 'postgres' | 'mariadb' | 'mongodb';
--- src/public/DatabaseModule.ts
+++ src/public/DatabaseModule.ts
@@ -3,20 +3,43 @@ import { createConnection } from 'typeorm';
 import { EasyExpressServer, IEasyExpressAttachableModule } from '@easy-express/server';
 import fs from 'fs';
 
+/**
+ * A database module connects to the database using the environment variable credentials and
+ * loads all TypeORM entities located at the given path.
+ * To connect to the database, one can attach this module to an EasyExpress Server
+ * or, if they wish to use this module without an EasyExpressServer, they can
+ * simply call the connect() method.
+ */
 export class DatabaseModule implements IEasyExpressAttachableModule {
   private pathToEntities: string;
   private logging: boolean;
 
+  /**
+   * Constructs a DatabaseModule that will read the TypeORM entities
+   * located at the given path.
+   * @param pathToEntities the path to the directory where all TypeORM entities are located in
+   * @param logging wherther or not you want this module to log all TypeORM operations
+   */
   constructor(pathToEntities: string, logging?: boolean) {
     this.pathToEntities = pathToEntities;
     this.logging = logging !== undefined ? logging : false;
   }
 
+  /**
+   * 'Attaches' this module to the given server. There isn't much interaction
+   * between this module and an EasyExpressServer, so this method is really only to make
+   * module use consistent -- it only calls this.connect().
+   * @param server the server to 'attach' to (doesn't matter)
+   */
   public attachTo(server: EasyExpressServer): Promise<unknown> {
     return this.connect();
   }
 
-  private async connect() {
+  /**
+   * Connects to the database and initializes TypeORM with the entities
+   * at the given directory.
+   */
+  public async connect() {
     const entities: any = await this.loadFiles<any>(this.pathToEntities);
     // refer to https://typeorm.io/#/ to view how to use the connection
     return createConnection({
@@ -39,6 +62,12 @@ export class DatabaseModule implements IEasyExpressAttachableModule {
       });
   }
 
+  /**
+   * Loads all files inside a given directory and returns them in a list of
+   * the given type.
+   *
+   * @param path the path to a directory containing the files of type <T>
+   */
   private async loadFiles<T>(path: string): Promise<T[]> {
     return new Promise((resolve, reject) => {
       fs.readdir(path, async (err, filenames) => {
